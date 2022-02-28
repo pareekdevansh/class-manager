5c14d694152b88dd464af0e4cce403c8a13ceb1a
pareekdevansh <devanshpareek863@gmail.com>
2022-02-28 12:12:10 +0530

    Project bug resolved

---
+++ app/src/main/java/com/pareekdevansh/classmanager/repository/AppRepository.kt
@@ -0,0 +1,23 @@
+package com.pareekdevansh.classmanager.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
+import java.util.*
+
+class AppRepository {
+
+    companion object{
+        var auth = Firebase.auth
+        var calendar : Calendar = Calendar.getInstance()
+        val userCollectoinRef = FirebaseFirestore.getInstance().collection("user")
+        val lectureCollectoinRef = FirebaseFirestore.getInstance().collection("lectures")
+        val currDay = calendar.get(Calendar.DAY_OF_WEEK)
+
+    }
+
+}
